b30370126d403af0bcf2c754914807b4c9bc64eb
lgad31vn <66233296+lgad31vn@users.noreply.github.com>
2021-04-11 02:34:55 -0500

    Add files via upload

---
+++ SyntaxChecker.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+public class SyntaxChecker {
+
+    private String expression; // expression to validate
+    private Stack<String> stack; // holds opening symbols ONLY
+    private String open; // opening symbols
+    private String close; // closing symbols
+
+    // TODO: initialize the instance variables expression, stack, open, close
+    public SyntaxChecker(String open, String close, String exp)
+    {
+        //same name using this.open for indicating private variables
+        stack = new Stack<>();
+        expression = exp;
+        this.close = close;
+        this.open = open;
+
+    }
+
+    // TODO complete the checkExpressions method that determines if the expression
+    // has a closing symbol for every opening symbol
+    // @return true if the expression is valid, false otherwise
+    public boolean checkExpression()
+    {
+
+        boolean result = false;
+        String[] closeArr = close.split("");
+        String[] openArr = open.split("");
+        String[] expArr = expression.split("");
+        int indexClose=0;
+
+        for (String expCh : expArr) {
+            for (String o : openArr) {
+                if (expCh.equals(o)) {
+                    stack.push(expCh);
+
+                }
+            }
+            for (String l : closeArr) {
+                if (expCh.equals(l)) {
+                    indexClose = close.indexOf(expCh);
+
+                    if (stack.isEmpty()) {
+                        return false;
+                    } else if (indexClose != open.indexOf(stack.peek())){
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+        }
+        if (stack.isEmpty()) {
+            result = true;
+        }
+
+
+        return result;
+
+    }
+
+
+    // This method calls the checkExpression method and will determine if the
+    // expression is valid (every opening symbol, has a closing symbol)
+    // A string is return with a validation message
+    // @return a validation message
+    public String validate()
+    {
+        String result="";
+
+        if (checkExpression()==true)
+            result += expression + " is correct\n";
+        else
+            result += expression + " is incorrect\n";
+
+
+        return result;
+    }
+
+
+    public String toString()
+    {
+        return "Expression: " + expression + "\nStack: " + stack + "\nOpen: " + open + "Close: " + close;
+    }
+
+}
---
+++ SyntaxCheckerClient.java
@@ -0,0 +1,34 @@
+public class SyntaxCheckerClient
+{
+
+    public static void main(String[] args)
+    {
+
+        SyntaxChecker test = new SyntaxChecker("{(<[","})>]","(abc(*def)");
+        System.out.println(test.validate());
+
+        SyntaxChecker test2 = new SyntaxChecker("{(<[","})>]","[{}]");
+        System.out.println(test2.validate());
+
+        SyntaxChecker test3 = new SyntaxChecker("{(<[","})>]","[");
+        System.out.println(test3.validate());
+
+        SyntaxChecker test4 = new SyntaxChecker("{(<[","})>]","[{<()>}]");
+        System.out.println(test4.validate());
+
+        SyntaxChecker test5 = new SyntaxChecker("{(<[","})>]","{<html[value=4]*(12)>{$x}}");
+        System.out.println(test5.validate());
+
+        SyntaxChecker test6 = new SyntaxChecker("{(<[","})>]","[one]<two>{three}(four)");
+        System.out.println(test6.validate());
+
+        SyntaxChecker test7 = new SyntaxChecker("{(<[","})>]","car(cdr(a)(b)))");
+        System.out.println(test7.validate());
+
+        SyntaxChecker test8 = new SyntaxChecker("{(<[","})>]","car(cdr(a)(b))");
+        System.out.println(test8.validate());
+
+
+    }
+
+}
